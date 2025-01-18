63751ed0a301bf7c08db23de2e00b341e9918e9e
Jhordy Rosario <jhordyrosario@gmail.com>
2025-01-17 23:50:28 -0400

    [FEAT] #0: Add project basic structure
    - Adding header component
    
    Refs: 0
    User: Jhordy Rosario jhordyrosario@gmail.com

---
+++ src/assets/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <h1>Commit Message Utility</h1>
+    <p>Create commit messages using conventional commits specification</p>
+  </header>
+);
+
+export default Header;
