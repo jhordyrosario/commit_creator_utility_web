ae1afa4fef75b7b5d0f555bfe9b970bafc648e8b
Jhordy Rosario <jhordyrosario@gmail.com>
2025-01-21 10:08:51 -0400

    [FEAT] #0: Add project basic structure
    - Adding GeneratedCommitDisplay Component
    
    Refs: 0
    User: Jhordy Rosario jhordyrosario@gmail.com

---
+++ src/components/GeneratedCommitDisplay.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const GeneratedCommitDisplay = ({ message }) => (
+    <div className="generated-commit-display">
+        <label>Generated Commit Message:</label>
+        <pre>{commitMessage}</pre>
+    </div>
+);
+
+export default GeneratedCommitDisplay;
