12f38158e40f88cfab8d0d30226b56f3d26ebd33
Jhordy Rosario <jhordyrosario@gmail.com>
2025-01-30 20:43:42 -0400

    [FEAT] #0: Add project basic structure
    - Fixing DetailedDescriptionInput component
    
    Refs: 0
    User: Jhordy Rosario jhordyrosario@gmail.com

--- src/components/DetailedDescriptionInput.jsx
+++ src/components/DetailedDescriptionInput.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const DetailedDescriptionInput = ({details, onchange}) => (
+const DetailedDescriptionInput = ({details, onChange}) => (
     <div>
         <label>Detailed Description (Optional):</label>
         <textarea
             placeholder="Write a detailed description..."
             value={details}
-            onChange={(e) => onchange(e.target.value)}
+            onChange={(e) => onChange(e.target.value)}
         />
     </div>
 );
